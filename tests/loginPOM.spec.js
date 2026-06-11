916919f83d496de348909a50dcecb15148eef63e
akashbhanu_tiwari <akashbhanutiwari@gmail.com>
2026-06-11 17:56:55 +0530

    first commit

--- tests/loginPOM.spec.js
+++ tests/loginPOM.spec.js
@@ -10,7 +10,7 @@ test('@Login Login POM test', async ({ page }) => {
     await login.loginToApplication('tomsmith', 'SuperSecretPassword!');
 });
 
-
+// use the context to share the browser instance across multiple tests
 test('context test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
